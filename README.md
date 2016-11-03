b1a942d88bfad577a9aa01598981fc775b5c6e07
trangsinh87 <trangsinh87@yahoo.com.vn>
2016-11-03 17:39:27 +0700

    init

---
+++ README.md
@@ -0,0 +1,14 @@
+# gulp-webpack-react-babel
+
+## Building
+For development...
+
+```
+gulp dev
+open http://localhost:5000
+```
+
+For production
+```
+gulp build --production
+```
---
+++ src/js/app.jsx
@@ -0,0 +1,10 @@
+/* @flow */
+import 'regenerator/runtime';
+import React from 'react';
+import { render } from 'react-dom';
+import ExampleApp from './ExampleApp.jsx';
+
+render(
+    <ExampleApp />,
+    document.getElementById('app')
+);
